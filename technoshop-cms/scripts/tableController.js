08f35e41aaba1539e14aa820af86093e487e3c07
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-08-17 14:35:01 +0200

    delete goods

--- technoshop-cms/scripts/tableController.js
+++ technoshop-cms/scripts/tableController.js
@@ -1,5 +1,5 @@
 import { tableRender } from "./tableView.js";
-import { getGoods } from "./serviceApi.js";
+import { getGoods, deleteGoods } from "./serviceApi.js";
 import { modalController } from "./modalController.js";
 import { tableGoods} from "./elems.js";
 
@@ -11,7 +11,19 @@ export const tableController = async () => {
       target: '.table-goods-item',
       targetExclude: '.btn-delete',
     }
-  })
+  }); 
+tableGoods.addEventListener('click', async ({target}) => {
+  const delBtn = target.closest('.btn-delete');
+  if (delBtn) {
+    const row = delBtn.closest('.table-goods-item');
+    const isDel = await deleteGoods(row.dataset.id);
+
+    if (isDel) {
+      row.remove();
+    }
+  }
+})
+
   const goods = await getGoods();
   tableRender(goods);
 };
